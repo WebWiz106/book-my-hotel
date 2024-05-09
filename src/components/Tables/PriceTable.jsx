66e5d2d8cc6b0676525a792eb8f2ae71607f6738
maynat123 <maynatrajput511999@gmail.com>
2024-05-08 22:17:55 -0700

    inventory table

--- src/components/Tables/PriceTable.jsx
+++ src/components/Tables/PriceTable.jsx
@@ -1,56 +1,13 @@
-import React, { useState } from 'react'
-import { BsThreeDots } from "react-icons/bs";
+import React from 'react'
 import { IoSync } from "react-icons/io5";
 import { GiBackwardTime } from "react-icons/gi";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
-import InventoryTable from './Tables/InventoryTable';
-import PriceTable from './Tables/PriceTable';
 
-
-const Mange = () => {
-  const [showInventory, setShowInventory] = useState(false);
-  const [showPrice, setShowPrice] = useState(false);
-
-  const handleInventoryClick = () => {
-    setShowPrice(false);
-    setShowInventory(true)
-  }
-  const handlePriceClick = () => {
-    setShowInventory(false)
-    setShowPrice(true);
-  }
-
+const PriceTable = () => {
   return (
-    <div className='maxwidth px-5 mx-auto mt-4'>
-      <div className='flex justify-between'>
-        <div class="inline-flex rounded-md shadow-sm" role="group">
-          <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-            All
-          </button>
-          <button onClick={handleInventoryClick} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-            Inventory
-          </button>
-          <button onClick={handlePriceClick} type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-            Price
-          </button>
-        </div>
-        <div>
-          <button className='px-4 py-2 text-sm font-medium text-blue-400 bg-white border border-blue-400 rounded-lg hover:bg-blue-400 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700'><BsThreeDots size={22} /></button>
-        </div>
-      </div>
-      {
-        showInventory && <>
-          <InventoryTable />
-        </>
-      }
-      {
-        showPrice && <>
-          <PriceTable />
-        </>
-      }
-      {/* <div className='mt-4'>
+    <div className='mt-4'>
         <div class="relative overflow-x-auto">
           <table class="w-full text-sm text-left rtl:text-right text-gray-500 border-2 border-gray-300 rounded-md">
             <thead class="text-xs text-gray-700 uppercase">
@@ -184,70 +141,44 @@ const Mange = () => {
                   </div>
                 </th>
                 <td class="w-full flex">
+                  <div className='flex flex-col justify-center py-2 px-[10px] h-full'>
+                    <span className="rounded-md mb-[3px] mt-6">3988/-</span>
+                  </div>
                   <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">1</span>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">4</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">3</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">6</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">8</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">2</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">6</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
+                  <div className='flex flex-col justify-end py-2 px-[10px] h-full'>
+                    <span className="h-[8px] rounded-md mb-[3px] mt-6">3988/-</span>
                   </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
-                  </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
-                  </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
-                  </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
-                  </div>
-                  <div className='flex flex-col justify-end py-2 px-[10px] h-full border-l-2 border-white'>
-                    <span className="bg-green-600 h-[8px] rounded-md mb-[3px] mt-6"></span>
-                    <span className="border-2 border-gray-300 px-4 py-1 rounded-md">5</span>
-                  </div>
+                  
+                  
                 </td>
               </tr>
               <tr class="bg-white border-t-2 border-gray-300">
@@ -417,9 +348,8 @@ const Mange = () => {
             </tbody>
           </table>
         </div>
-      </div> */}
-    </div>
+      </div>
   )
 }
 
-export default Mange
+export default PriceTable
